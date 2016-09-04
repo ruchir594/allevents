0f698fd3dff7e9117592d28a2352c549d8c67213
ruchir594 <ruchir594@gmail.com>
2016-09-04 11:27:32 +0530

    learning updated to check w2v in aws

--- learning.py
+++ learning.py
@@ -1,6 +1,26 @@
+import sys
+sys.path.insert(0, './head')
 import numpy
+from scipy import spatial
+import word2vec
 a = numpy.loadtxt('latent_model.txt')
 with open('latent_tags.txt') as f:
     b = f.readlines()
 print len(a)
 print len(b)
+def matcher(line, context):
+    model = word2vec.load('../lib/word2vec/vectors.bin')
+    #clusters = word2vec.load_clusters('../lib/word2vec/text8-clusters.txt')
+    a = numpy.loadtxt('latent_model.txt')
+    with open('latent_tags.txt') as f:
+        b = f.readlines()
+
+
+    a = model['love']
+    print a
+    b = model['hate']
+    print b
+    result = 1 - spatial.distance.cosine(a, b)
+    print result
+    return 'jankiap50'
+matcher('a', 0)
