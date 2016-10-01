4f8f9d67c450b3f5873eab7e50ab7bda15d1baba
ruchir594 <ruchir594@gmail.com>
2016-10-01 13:08:35 +0530

    demo function 2

---
+++ allevents_aws.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// Getting started with Facebook Messaging Platform
+// https://developers.facebook.com/docs/messenger-platform/quickstart
+
+const express = require('express');
+const request = require('superagent');
+const bodyParser = require('body-parser');
+const https = require('https');
+
+var spawn = require("child_process").spawn;
+var PythonShell = require('python-shell');
+
+// Variables
+//let pageToken = "";
+let pageToken = "EAAJffei4MlEBAAsOcdoRvIXuvZADZAIpCDqEozNZB5OcyKZBZCCn4yWU7lCLut0hGyZCHpNBjVohXbE8TwunqnwZA1nWivurifm9iMVyM74pNJvwg0WC9UriuJABn0crPMvyRm6sFzI8BfWeQ8nXWDiyJgPs4qw2fP84N7JEuE9FwZDZD";
+const verifyToken = "newton";
+const privkey = "/etc/letsencrypt/live/yelper.tonatasha.com/privkey.pem";
+const cert = "/etc/letsencrypt/live/yelper.tonatasha.com/cert.pem";
+const chain = "/etc/letsencrypt/live/yelper.tonatasha.com/chain.pem";
+
+const app = express();
+const fs = require('fs');
+
+app.use(bodyParser.json());
+
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.verify_token'] === verifyToken) {
+        return res.send(req.query['hub.challenge']);
+    }
+    res.send('Error, wrong validation token');
+});
+app.post('/webhook', (req, res) => {
+    const messagingEvents = req.body.entry[0].messaging;
+
+    messagingEvents.forEach((event) => {
+        const sender = event.sender.id;
+
+        if (event.postback) {
+            const text = JSON.stringify(event.postback).substring(0, 200);
+            //sendTextMessage(sender, 'Postback received: ' + text);
+        } else if (event.message && event.message.text) { // bracket 101 open
+            const text = event.message.text.trim().substring(0, 200);
+
+            res.send('Received POST request on /webhook successfully.');
+
+        } //bracket 101 close
+    });
+
+    res.sendStatus(200);
+});
+
+
+app.post('/token', (req, res) => {
+    if (req.body.verifyToken === verifyToken) {
+        pageToken = req.body.token;
+        return res.sendStatus(200);
+    }
+    res.sendStatus(403);
+});
+app.get('/token', (req, res) => {
+    if (req.body.verifyToken === verifyToken) {
+        return res.send({token: pageToken});
+    }
+    res.sendStatus(403);
+});
+
+ https.createServer({
+      key: fs.readFileSync(privkey),
+      cert: fs.readFileSync(cert),
+      ca: fs.readFileSync(chain)
+    }, app).listen(3600, function () {
+  console.log('App is running on port 3600');
+});
