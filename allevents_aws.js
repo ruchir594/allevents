621bd8dbf982318ec6d7a2a0b48f3bc4136f0da6
ruchir594 <ruchir594@gmail.com>
2016-10-02 12:39:06 +0530

    get req send message test 4

--- allevents_aws.js
+++ allevents_aws.js
@@ -142,7 +142,7 @@ app.get('/webhook', (req, res) => {
                 var places = results.split("^");
                 //console.log(places)
                 if (places[0] == 'jankiap50') {
-                    res.send({userid:sender, response:places[1]});
+                    res.send({userid:sender, response:{text:places[1]}});
                     //res.send({userid:sender, response:{text: "Hmmm something is wrong. I will report it, meanwhile, try searching something else?")
                     //res.send({userid:sender, response:{text: places[2]);
                     //res.send({userid:sender, response:{text: places[3])
