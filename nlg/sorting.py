3fd58e4e757a004bcf5e484e6a6943fcdb96d483
ruchir594 <ruchir594@gmail.com>
2016-11-05 21:43:59 +0530

    added full month shortening

--- nlg/sorting.py
+++ nlg/sorting.py
@@ -51,6 +51,8 @@ def get_per_day(a, e):
     if len(a.split(' ')) == 2:
         ad = a.split(' ')
         m = ad[0].title()
+        if len(m) > 3:
+            m = m[:3]
         a = ad[1]
         if a < 10:
             a = '0'+str(a)
